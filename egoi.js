62752fd666bced0382f447900569d980123a0a50
Daniel <daniel.sendmemail@gmail.com>
2017-12-02 10:28:56 -0500

    franciso cardona ventas

---
+++ egoi.js
@@ -0,0 +1,15 @@
+var _egoiaq = _egoiaq || [];
+(function(){
+	var u=(("https:" == document.location.protocol) ? "https://egoimmerce.e-goi.com/" : "http://egoimmerce.e-goi.com/");
+	_egoiaq.push(['setClientId', "264069"]);
+	_egoiaq.push(['setTrackerUrl', u+'collect']);
+	_egoiaq.push(['trackPageView']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.type='text/javascript';
+	g.defer=true;
+	g.async=true;
+	g.src=u+'egoimmerce.js';
+	s.parentNode.insertBefore(g,s);
+})();
+
+						
